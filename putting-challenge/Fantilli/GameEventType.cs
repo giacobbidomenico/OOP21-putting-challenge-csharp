ae674bc8709572c0f3a0138d22eebf2404865fc4
GiorgioFantilli <giorgio.fantilli@studio.unibo.it>
2022-07-07 19:40:33 +0200

    feat: implement game event type

---
+++ putting-challenge/Fantilli/GameEventType.cs
@@ -0,0 +1,54 @@
+﻿namespace putting_challenge.Fantilli
+{
+    /// <summary>
+    /// Enumeration that establishes the various types of events occurred in the Application, 
+    /// to exchange between Model, Controller and View.
+    /// </summary>
+    public enum GameEventType
+    {
+        /// <summary>
+        /// Require to the application the start of the game.
+        /// </summary>
+        START,
+
+        /// <summary>
+        /// Notify the application that the game is over.
+        /// </summary>
+        GAMEOVER,
+
+        /// <summary>
+        /// Notify that the player shot the ball.
+        /// </summary>
+        SHOOT,
+
+        /// <summary>
+        /// Notify the application that the game is won.
+        /// </summary>
+        WIN,
+
+        /// <summary>
+        /// Require to the application to show the leader-board.
+        /// </summary>
+        SHOW_LEADERBOARD,
+
+        /// <summary>
+        /// Require to the application to show the main menu.
+        /// </summary>
+        SHOW_MAIN_MENU,
+
+        /// <summary>
+        /// Require to set a specified scene.
+        /// </summary>
+        SET_SCENE,
+
+        /// <summary>
+        /// Require to update the game statistics (lives and score).
+        /// </summary>
+        UPDATE_STATS,
+
+        /// <summary>
+        /// Require to shutdown the entire application.
+        /// </summary>
+        QUIT
+    }
+}
