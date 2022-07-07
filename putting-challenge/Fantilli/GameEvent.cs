6e466fd21fefd8cdb03678ebccf7519da223bacc
GiorgioFantilli <giorgio.fantilli@studio.unibo.it>
2022-07-07 19:06:58 +0200

    feat: implement game events interface

---
+++ putting-challenge/Fantilli/GameEvent.cs
@@ -0,0 +1,21 @@
+﻿namespace putting_challenge.Fantilli
+{
+    using Optional;
+
+    /// <summary>
+    /// Interface that represent a game event. Indicates that a colleague-defined event has occurred.
+    /// When a colleague wants to interact with other colleagues, it calls the Mediator passing the event occured.
+    /// The Mediator proceeds to notify the other colleagues passing them the event.
+    /// </summary>
+    public interface GameEvent
+    {
+        /// <summary>
+        /// Gets the type of the event.
+        /// </summary>
+        GameEventType EventType { get; }
+
+        /// <typeparam name="T">unused</typeparam>
+        /// <returns>an empty <see cref="Option"/>.</returns>
+        Option<T> GetDetails<T>();
+    }
+}
