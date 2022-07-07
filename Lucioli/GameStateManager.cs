9434772dfc93dff46211844bf9560808616152ff
Luca Lucioli <luca.lucioli@studio.unibo.it>
2022-07-07 14:49:43 +0200

    feat: add GameStateManager

---
+++ Lucioli/GameStateManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Lucioli
+{
+    public class GameStateManager
+    {
+        private Mediator _generalMediator;
+        private const GameStatus _initialState = GameStatus.MainMenu;
+        public GameState CurrentGameState
+        {
+            get;
+            private set;
+        }
+        ///<inheritdoc/>
+        public void InitState()
+        {
+            CurrentGameState = new ScreenGameState(this, _initialState);
+        }
+        ///<inheritdoc/>
+        public void SwitchState(GameStatus newStatus)
+        {
+            switch (newStatus)
+            {
+                case GameStatus.Play:
+                    CurrentGameState = new GamePlayGameState(this, newStatus);
+                    // missing
+                    break;
+                case GameStatus.GameOver:
+                case GameStatus.GameWin:
+                case GameStatus.Leaderboard:
+                case GameStatus.MainMenu:
+                    CurrentGameState = new ScreenGameState(this, newStatus);
+                    // missing
+                    break;
+                default:
+                    break;
+            }
+        }
+        ///<inheritdoc/>
+        public void Update(long dt)
+        {
+            if(CurrentGameState.Environment != null)
+            {
+                CurrentGameState.Environment.Update(dt);
+                CurrentGameState.ReceiveEvents();
+            }
+        }
+
+
+    }
+}
