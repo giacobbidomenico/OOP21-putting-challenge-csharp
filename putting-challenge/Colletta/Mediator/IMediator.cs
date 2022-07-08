16a3a959b9d008542135c095cdfb676d691ce669
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2022-07-08 21:21:48 +0200

    feat: define Imediator interface

---
+++ putting-challenge/Colletta/Mediator/IMediator.cs
@@ -0,0 +1,25 @@
+﻿using putting_challenge.Fantilli;
+
+namespace PuttingChallenge.Colletta.Mediator
+{
+    public interface IMediator
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="newColleague"></param>
+        void AddColleague(Colleague newColleague);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="toRemove"></param>
+        void RemoveColleague(Colleague toRemove);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name=""></param>
+        void NotifyColleagues(IGameEvent event, IColleague sender);
+    }
+}
