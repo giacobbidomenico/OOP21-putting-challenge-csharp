c8193fcf2fe3036ff1b6e591bd0cfc42e1fa7d29
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2022-07-08 17:19:24 +0200

    feat: introduce IPassiveCircleBoundingBox interface

---
+++ putting-challenge/Colletta/IPassiveCircleBoundingBox.cs
@@ -0,0 +1,13 @@
+﻿using putting_challenge.Fantilli;
+
+namespace OOP21-putting-challenge-csharp.Colletta
+{
+
+    public interface IPassiveCircleBoundingBox
+    {
+
+        double Radius { get; }
+
+        Point2D Position { get; set; }
+    }
+}
