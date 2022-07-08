f45c807514023ee157ff4032427697cb9b7c4521
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2022-07-08 23:00:37 +0200

    docs: refine ConcreteMediator documentation

--- putting-challenge/Colletta/Mediator/ConcreteMediator.cs
+++ putting-challenge/Colletta/Mediator/ConcreteMediator.cs
@@ -1,22 +1,28 @@
 ﻿using putting_challenge.Fantilli;
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace PuttingChallenge.Colletta.Mediator
 {
+    /// <summary>
+    /// Implementation of <see cref="IMediator"/>
+    /// </summary>
     public class ConcreteMediator : IMediator
     {
         private readonly IList<IColleague> _colleagues;
         
+        /// <summary>
+        /// Builds a <see cref="ConcreteMediator"/>
+        /// </summary>
         public ConcreteMediator()
         {
             List<IColleague> ts = new List<IColleague>();
             _colleagues = ts;
         }
 
+        /// <inheritdoc/>
         public void AddColleague(IColleague newColleague) => _colleagues.Add(newColleague);
 
+        /// <inheritdoc/>
         public void NotifyColleagues(IGameEvent eventOccurred, IColleague sender)
         {
             foreach (IColleague colleague in _colleagues)
@@ -28,6 +34,7 @@ namespace PuttingChallenge.Colletta.Mediator
             }
         }
 
+        /// <inheritdoc/>
         public void RemoveColleague(IColleague toRemove) => _colleagues.Remove(toRemove);
     }
 }
