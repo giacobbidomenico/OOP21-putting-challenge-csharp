d971850bb2c4e542e479b6097938074b6fa0a5d5
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2022-07-09 01:15:54 +0200

    feat: define ConcertePassiveCircleBoundingBox unimplemented methods

---
+++ putting-challenge/Colletta/Collisions/ConcretePassiveCircleBoundingBox.cs
@@ -0,0 +1,23 @@
+﻿using puttingchallenge.Fantilli.common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PuttingChallenge.Colletta.Collisions
+{
+    public class ConcretePassiveCircleBoundingBox : IPassiveCircleBoundingBox
+    {
+        private readonly double _radius;
+        private Point2D _centerPosition;
+        
+        public ConcretePassiveCircleBoundingBox(Point2D centerPosition, double radius)
+        {
+            _centerPosition = centerPosition;
+            _radius = radius;
+        }
+
+        public double Radius => _radius;
+
+        public Point2D Position { get => _centerPosition; set => _centerPosition = value; }
+}
+}
