6e4a44a61b7da914cfc378f65c3672f24427a99d
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2022-07-08 23:37:08 +0200

    feat: define IActiveBoundingBox implementation, AxisAlignedBoundingBox

---
+++ putting-challenge/Colletta/Collisions/AxisAlignedBoundingBox.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PuttingChallenge.Colletta.Collisions
+{
+    class AxisAlignedBoundingBox : IActiveBoundingBox
+    {
+        /// <inheritdoc cref="IActiveBoundingBox.BounceAlongTangent"/>
+        public bool BounceAlongTangent()
+        {
+        }
+
+        /// <inheritdoc cref="IActiveBoundingBox.ClosestPointOnBBToPoint(Point2D)"/>
+        public Point2D ClosestPointOnBBToPoint(Point2D point)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc cref="IActiveBoundingBox.GetNormal(Point2D)"/>
+        public Vector2D GetNormal(Point2D pointOnActiveBoundingBox)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc cref="IActiveBoundingBox.IntersectionToSegment(Point2D, Point2D)"/>
+        public Point2D IntersectionToSegment(Point2D pointA, Point2D pointB)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc cref="IActiveBoundingBox.IsColliding(IPassiveCircleBoundingBox)"/>
+        public bool IsColliding(IPassiveCircleBoundingBox circle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
