3976ed73a23bfa2fbac4804a5dc5a357f51aa838
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2022-07-08 21:39:22 +0200

    feat: define IColleague interface

---
+++ putting-challenge/Colletta/Mediator/IColleague.cs
@@ -0,0 +1,15 @@
+﻿using putting_challenge.Fantilli;
+
+namespace PuttingChallenge.Colletta.Mediator
+{
+    public interface IColleague
+    {
+        /// <summary>
+        /// Sets the mediator to delegate event notification.
+        /// </summary>
+        IMediator Mediator { set; };
+
+        void NotifyEvent(IGameEvent eventOccurred);
+
+    }
+}
