0d7fa13749ff821d6e49f3b95412bcf2d9ed549e
Luca Lucioli <luca.lucioli@studio.unibo.it>
2022-07-08 12:58:57 +0200

    feat: add ScreenGameState

---
+++ putting-challenge/Lucioli/ScreenGameState.cs
@@ -0,0 +1,29 @@
+﻿using putting_challenge.Fantilli;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace putting_challenge.Lucioli
+{
+    public class ScreenGameState : GameState
+    {
+        public ScreenGameState(GameStateManager manager, GameStatus status) : base(manager, status)
+        {
+        }
+
+        public override Tuple<SceneType, List<IGameObject>> InitState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void NotifyEvents(ModelEventType eventType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ReceiveEvents()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
