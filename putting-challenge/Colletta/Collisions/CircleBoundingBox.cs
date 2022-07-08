e0d105dfccc6a7032b60beb9b5c8205eb8f5f3cb
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2022-07-09 01:40:08 +0200

    docs: refine CircleBoundingBox documentation

--- putting-challenge/Colletta/Collisions/CircleBoundingBox.cs
+++ putting-challenge/Colletta/Collisions/CircleBoundingBox.cs
@@ -5,19 +5,29 @@ using System.Text;
 
 namespace PuttingChallenge.Colletta.Collisions
 {
+    /// <summary>
+    /// Represents an active circle bounding box.
+    /// </summary>    
     public class CircleBoundingBox : IActiveBoundingBox
     {
         private readonly Point2D _centerPosition;
         private readonly double _radius;
 
+        /// <summary>
+        /// Builds a <see cref="CircleBoundingBox"/>.
+        /// </summary>
+        /// <param name="position">Position of the center of the bounding box,</param>
+        /// <param name="radius">Radius of the boudning box</param>
         public CircleBoundingBox(Point2D position, double radius)
         {
             _centerPosition = position;
             _radius = radius;
         }
 
+        /// <inheritdoc cref="IActiveBoundingBox.BounceAlongTangent"/>
         public bool BounceAlongTangent() => true;
 
+        /// <inheritdoc cref="IActiveBoundingBox.ClosestPointOnBBToPoint"/>
         public Point2D ClosestPointOnBBToPoint(Point2D point)
         {
             Point2D diff = new Point2D(
@@ -31,6 +41,7 @@ namespace PuttingChallenge.Colletta.Collisions
                     _centerPosition.Y + (diff.Y / module * _radius));
         }
 
+        /// <inheritdoc cref="IActiveBoundingBox.GetNormal"/>
         public Vector2D GetNormal(Point2D pointOnActiveBoundingBox)
         {
             double x = -_centerPosition.X + pointOnActiveBoundingBox.X;
@@ -38,6 +49,7 @@ namespace PuttingChallenge.Colletta.Collisions
             return new Vector2D(x / Math.Sqrt(y * y + x * x), y / Math.Sqrt(y * y + x * x));
         }
 
+        /// <inheritdoc cref="IActiveBoundingBox.IntersectionToSegment"/>
         public Point2D IntersectionToSegment(Point2D pointA, Point2D pointB)
         {
             if (ClosestPointOnBBToPoint(pointA) == pointA || ClosestPointOnBBToPoint(pointB) != pointB)
@@ -61,6 +73,7 @@ namespace PuttingChallenge.Colletta.Collisions
             return new Point2D(resultX, resultY);
         }
 
+        /// <inheritdoc cref="IActiveBoundingBox.IsColliding"/>
         public bool IsColliding(IPassiveCircleBoundingBox circle)
         {
             return _radius + circle.Radius >= Point2D.GetDistance(_centerPosition, circle.Position));
