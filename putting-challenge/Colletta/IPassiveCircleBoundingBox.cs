d71eed808a80dfa7cb876f595cf3ea7f0b847ffe
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2022-07-08 17:26:57 +0200

    docs: define documentstion of IPassiveCircleBoundingBox interface

--- putting-challenge/Colletta/IPassiveCircleBoundingBox.cs
+++ putting-challenge/Colletta/IPassiveCircleBoundingBox.cs
@@ -2,12 +2,20 @@
 
 namespace OOP21-putting-challenge-csharp.Colletta
 {
-
+    /// <summary>
+    /// Represents a basic circle bounsing box.
+    /// </summary>
     public interface IPassiveCircleBoundingBox
     {
 
+    /// <summary>
+    /// Gets the radius of the bounding box.
+    /// </summary>
         double Radius { get; }
 
+    /// <summary>
+    /// Gets or sets the position of the bounding box.
+    /// </summary>
         Point2D Position { get; set; }
     }
 }
