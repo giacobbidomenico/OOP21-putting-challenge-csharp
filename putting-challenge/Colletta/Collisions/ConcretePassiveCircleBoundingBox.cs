a13bfcef381facc03528f7c3aea91e76a69def71
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2022-07-09 01:20:35 +0200

    docs: refine ConcretePassiveCircleBoundingBox documentation

--- putting-challenge/Colletta/Collisions/ConcretePassiveCircleBoundingBox.cs
+++ putting-challenge/Colletta/Collisions/ConcretePassiveCircleBoundingBox.cs
@@ -5,19 +5,29 @@ using System.Text;
 
 namespace PuttingChallenge.Colletta.Collisions
 {
+    /// <summary>
+    /// Represents a concrete <see cref="IPassiveCircleBoundingBox"-/>
+    /// </summary>
     public class ConcretePassiveCircleBoundingBox : IPassiveCircleBoundingBox
     {
         private readonly double _radius;
         private Point2D _centerPosition;
-        
+
+        /// <summary>
+        /// Builds a <see cref="ConcretePassiveCircleBoundingBox"/>
+        /// </summary>
+        /// <param name="centerPosition">Position of the center of the circumference.</param>
+        /// <param name="radius">Radius of the circumference.</param>
         public ConcretePassiveCircleBoundingBox(Point2D centerPosition, double radius)
         {
             _centerPosition = centerPosition;
             _radius = radius;
         }
 
+        /// <inheritdoc cref="IPassiveCircleBoundingBox.Radius"/>
         public double Radius => _radius;
 
+        /// <inheritdoc cref="IPassiveCircleBoundingBox.Position"/>
         public Point2D Position { get => _centerPosition; set => _centerPosition = value; }
 }
 }
