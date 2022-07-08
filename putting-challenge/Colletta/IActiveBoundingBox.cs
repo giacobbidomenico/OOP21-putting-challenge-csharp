512cbfc074b6a6a3b51419a70496166e882533cd
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2022-07-08 17:01:04 +0200

    docs: define documentation

--- putting-challenge/Colletta/IActiveBoundingBox.cs
+++ putting-challenge/Colletta/IActiveBoundingBox.cs
@@ -12,15 +12,32 @@ namespace OOP21-putting-challenge-csharp.Colletta
     /// Perform an intersection test with a <see cref="IPassiveCircleBoundingBox"/>.
     /// </summary>
     /// <param name="circle">The object to test with.</param>
-    /// <returns><see langword="true"/> if a collision is occurred, <see langword="false"/> otherwise.</returns>
+    /// <returns>True if a collision is occurred, false otherwise.</returns>
     bool IsColliding(IPassiveCircleBoundingBox circle);
 
+    /// <summary>
+    /// </summary>
+    /// <returns>True if an object colliding would bounce along the tangent, false otherwise</returns>
     bool BounceAlongTangent();
 
+    /// <summary>
+    /// </summary>
+    /// <param name="pointOnActiveBoundingBox">The point on the bounding box.</param>
+    /// <returns>The normal vector to the point passed</returns>
     Vector2D GetNormal(Point2D pointOnActiveBoundingBox);
 
+    /// <summary>
+    /// </summary>
+    /// <param name="point">The point to be projected to the bounding box.</param>
+    /// <returns>The closest point on the bounding box to point.</returns>
     Point2D ClosestPointOnBBToPoint(Point2D point);
 
+    /// <summary>
+    /// Performs an intersection test with a segment.
+    /// </summary>
+    /// <param name="pointA">Endpoint of the segment outside the bounding box.</param>
+    /// <param name="pointB">Endpoint of the segment inside the bounding box.</param>
+    /// <returns>The point of intersection with the segment.</returns>
     Point2D intersectionToSegment(Point2D pointA, Point2D pointB);
 
 }
