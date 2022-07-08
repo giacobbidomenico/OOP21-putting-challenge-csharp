79f525b0fd559ac7e006a7d7189bee67cecc85e8
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2022-07-09 01:56:49 +0200

    docs: refine DynamicBoundingBox interface

--- putting-challenge/Colletta/Collisions/IDynamicBoundingBox.cs
+++ putting-challenge/Colletta/Collisions/IDynamicBoundingBox.cs
@@ -7,9 +7,18 @@ using puttingchallenge.Fantilli.common;
 
 namespace PuttingChallenge.Colletta.Collisions
 {
+    /// <summary>
+    /// Represents a wrapprer for an <see cref="IActiveBoundingBox"/>
+    /// </summary>
     public interface IDynamicBoundingBox
     {
-
+        /// <summary>
+        /// Perform a dynamic intersection test between the wrapped object 
+        /// and a <see cref="IPassiveCircleBoundingBox"/>
+        /// </summary>
+        /// <param name="circle">Bounding box to perform the test with.</param>
+        /// <param name="dt">Time from last frame.</param>
+        /// <returns>A CollisionTest with the details about the test.</returns>
         ICollisionTest CollidesWith(IPassiveCircleBoundingBox circle, long dt);
 
         public interface ICollisionTest
