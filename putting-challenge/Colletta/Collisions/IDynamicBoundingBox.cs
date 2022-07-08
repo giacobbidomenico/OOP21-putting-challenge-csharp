45d41fe5c1b7f6ab36a90432ba2e086dfa34b408
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2022-07-09 01:52:58 +0200

    feat: intraduce IDynamicBonidngBox interface

---
+++ putting-challenge/Colletta/Collisions/IDynamicBoundingBox.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Optional;
+using Optional.Unsafe;
+using puttingchallenge.Fantilli.common;
+
+namespace PuttingChallenge.Colletta.Collisions
+{
+    public interface IDynamicBoundingBox
+    {
+
+        ICollisionTest CollidesWith(IPassiveCircleBoundingBox circle, long dt);
+
+        public interface ICollisionTest
+        {
+            Boolean IsColliding();
+
+            IActiveBoundingBox GetActiveBoundingBox();
+
+            Option<Point2D> GetEstimatedPointOfImpact();
+
+            Option<Vector2D> GetActiveBBSideNormal();
+
+            Option<Vector2D> GetActiveBBSideTanget();
+            
+            Option<Point2D> GetPassiveBoxPositionBeforeCollisions();
+        }
+    }
+}
