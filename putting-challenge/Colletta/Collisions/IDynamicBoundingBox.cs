836ab9b2a88effb8d1b592761f0eeac18fc5eb5b
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2022-07-09 02:02:21 +0200

    docs: refine CollisionTest documentation

--- putting-challenge/Colletta/Collisions/IDynamicBoundingBox.cs
+++ putting-challenge/Colletta/Collisions/IDynamicBoundingBox.cs
@@ -21,18 +21,40 @@ namespace PuttingChallenge.Colletta.Collisions
         /// <returns>A CollisionTest with the details about the test.</returns>
         ICollisionTest CollidesWith(IPassiveCircleBoundingBox circle, long dt);
 
+        /// <summary>
+        /// Represents a collision test between an active bounding box and a passive bounding box.
+        /// </summary>
         public interface ICollisionTest
         {
+            /// <summary>
+            /// </summary>
+            /// <returns>true if the collision has occurred, false otherwise.</returns>
             Boolean IsColliding();
 
+            /// <summary>
+            /// </summary>
+            /// <returns>The active bounding box related to the test.</returns>
             IActiveBoundingBox GetActiveBoundingBox();
 
+            /// <summary>
+            /// </summary>
+            /// <returns>The estimated point of impact, empty if the collision has not occurred.</returns>
             Option<Point2D> GetEstimatedPointOfImpact();
 
+            /// <summary>
+            /// </summary>
+            /// <returns>The normal of the colliding side of the passive bounding box.</returns>
             Option<Vector2D> GetActiveBBSideNormal();
 
+            /// <summary>
+            /// </summary>
+            /// <returns>The tangent to the point of collision.</returns>
             Option<Vector2D> GetActiveBBSideTanget();
             
+            /// <summary>
+            /// </summary>
+            /// <returns>The estimated position of the passive bounding box at the first time of collision, 
+            /// empty if the collision has not occurred.</returns>
             Option<Point2D> GetPassiveBoxPositionBeforeCollisions();
         }
     }
