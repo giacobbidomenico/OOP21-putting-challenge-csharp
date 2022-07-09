360521bec194eeabe63cd9ed473c844981d82689
Luca Lucioli <luca.lucioli@studio.unibo.it>
2022-07-09 14:31:42 +0200

    feat: add BatType enum

---
+++ putting-challenge/Lucioli/BatType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace putting_challenge.Lucioli
+{
+    public class DescriptorAttributeBatType : Attribute
+    {
+        public double Strength { get; }
+        public DescriptorAttributeBatType(double strength)
+        {
+            Strength = strength;
+        }
+    }
+    public enum BatType
+    {
+        [DescriptorAttributeBatType(strength: 1.4)]
+        Hybrid,
+        [DescriptorAttributeBatType(strength: 0.6)]
+        Wedge,
+        [DescriptorAttributeBatType(strength: 0.8)]
+        Iron,
+        [DescriptorAttributeBatType(strength: 0.2)]
+        Putter,
+        [DescriptorAttributeBatType(strength: 2)]
+        Wood
+    }
+}
